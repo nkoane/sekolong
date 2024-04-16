8e96e04923d1545cf03e37411322565605b89694
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-04-16 06:06:37 +0200

    some cosmetic work

--- classwork/im-iii/unboxing/main.js
+++ classwork/im-iii/unboxing/main.js
@@ -1,17 +1,47 @@
 const hero = document.getElementById('hero');
 const foto = document.getElementById('foto');
+const main = document.getElementById('app');
 
-hero.addEventListener('click', async function () {
-    foto.classList.add('hide');
+hero.addEventListener('click', async () => {
+  foto.classList.toggle('hide');
 
-    const randomNumber = Math.round(Math.random() * 993);
+  const pages = 10;
+  const page = Math.floor(Math.random() * pages);
+  const photos_url = `https://picsum.photos/v2/list?limit=100&page=${page}`;
 
-    const imageURL = `https://picsum.photos/v2/list?page=${randomNumber}&limit=1`;
+  const response = await fetch(photos_url);
 
-    const response = await fetch(imageURL);
-    const fotos = await response.json();
+  if (response.status === 200) {
 
-    foto.classList.remove('hide');
+    const photos = await response.json();
+    const photo = photos[Math.round(Math.random() * photos.length) - 1];
 
-    foto.src = `https://picsum.photos/id/${fotos[0].id}/1000/600`;
-});
+    const windowHeight = main.getBoundingClientRect().height;
+    const windowWidth = main.getBoundingClientRect().width;
+    //
+    const width = Math.max(320, Math.round(Math.random() * windowWidth));
+    const height = Math.max(320, Math.round(Math.random() * windowHeight));
+   
+    //
+    const photo_url = `https://picsum.photos/id/${photo.id}/${width}/${height}.jpg`;
+
+    console.log(photo, photo_url, width, height);
+
+    const img = new Image();
+
+    img.onload = () => {
+      foto.src = img.src;
+      hero.style.width = `${width}px`;
+      hero.style.height = `${height}px`;
+
+      foto.classList.toggle('hide');
+    }
+
+    img.onerror = (error) => {
+      console.log('failed to load image, try again.', error);
+    }
+
+    img.src = photo_url;
+
+  }
+})
