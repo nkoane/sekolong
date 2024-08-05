8b834520969846e9d43523d8abda46ce72b3a84f
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-08-05 16:03:10 +0200

    lesson 3 done.

--- classwork/im-iv/03/main.js
+++ classwork/im-iv/03/main.js
@@ -1,21 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('nav a');
-    Array.from(links).forEach((link, index) => {
+    const links = Array.from(document.querySelectorAll('nav a'));
+    for (const link of links) {
         link.addEventListener('click', () => {
-            Array.from(links).forEach((a) => a.classList.remove('selected'));
+            for (const a of links) a.classList.remove('selected');
             link.classList.add('selected');
         });
-    });
+    }
 
     document.addEventListener('scroll', () => {});
 
     let trackIndex = 0;
+
     document.addEventListener('keyup', (event) => {
-        console.log(event.key);
+        console.log(`You pressed: ${event.key}`);
         if (event.key === 'ArrowLeft') {
+            if (trackIndex === 0) return;
             trackIndex++;
             window.location.href = links[trackIndex].href;
         } else if (event.key === 'ArrowRight') {
+            if (trackIndex === links.length - 1) return;
             trackIndex--;
             window.location.href = links[trackIndex].href;
         }
