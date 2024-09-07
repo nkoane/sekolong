6ca1602960ea77ce068efc89e3549bd00969ba54
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-09-07 19:33:17 +0200

    Refactor event listeners and navigation logic in main.js
    Signed-off-by: lebogang nkoane <1555646+nkoane@users.noreply.github.com>

--- classwork/im-iv/03/main.js
+++ classwork/im-iv/03/main.js
@@ -1,26 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const links = Array.from(document.querySelectorAll('nav a'));
-    for (const link of links) {
-        link.addEventListener('click', () => {
-            for (const a of links) a.classList.remove('selected');
-            link.classList.add('selected');
-        });
-    }
-
-    document.addEventListener('scroll', () => {});
-
-    let trackIndex = 0;
-
-    document.addEventListener('keyup', (event) => {
-        console.log(`You pressed: ${event.key}`);
-        if (event.key === 'ArrowLeft') {
-            if (trackIndex === 0) return;
-            trackIndex++;
-            window.location.href = links[trackIndex].href;
-        } else if (event.key === 'ArrowRight') {
-            if (trackIndex === links.length - 1) return;
-            trackIndex--;
-            window.location.href = links[trackIndex].href;
-        }
+document.addEventListener("DOMContentLoaded", () => {
+  const links = Array.from(document.querySelectorAll("nav a"));
+  for (const link of links) {
+    link.addEventListener("click", () => {
+      for (const a of links) a.classList.remove("selected");
+      link.classList.add("selected");
     });
+  }
+
+  document.addEventListener("scroll", () => {});
+
+  let trackIndex = 0;
+
+  document.addEventListener("keyup", (event) => {
+    console.log(`You pressed: ${event.key}`);
+    if (event.key === "ArrowLeft") {
+      if (trackIndex === 0) return;
+      trackIndex++;
+      window.location.href = links[trackIndex].href;
+    } else if (event.key === "ArrowRight") {
+      if (trackIndex === links.length - 1) return;
+      trackIndex--;
+      window.location.href = links[trackIndex].href;
+    }
+  });
 });
