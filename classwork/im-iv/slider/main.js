d5538fd36cca754f02cd83cacfebfc2d9a8b2b17
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-10-21 12:23:52 +0200

    Refactor slider transition and add automatic sliding functionality

--- classwork/im-iv/slider/main.js
+++ classwork/im-iv/slider/main.js
@@ -1,22 +1,30 @@
-const slider = document.querySelector('section');
+document.addEventListener('DOMContentLoaded', () => {
+    const slider = document.querySelector('section');
 
-document.getElementById('next').onclick = () => {
-    const item = document.querySelector('section div:first-child');
-    slider.append(item);
-};
-document.getElementById('previous').onclick = () => {
-    const item = document.querySelector('section div:last-child');
-    slider.prepend(item);
-};
-
-document.addEventListener('keyup', (e) => {
-    if (e.key === 'ArrowRight') {
+    document.getElementById('next').onclick = () => {
         const item = document.querySelector('section div:first-child');
         slider.append(item);
-    }
-
-    if (e.key === 'ArrowLeft') {
+    };
+    document.getElementById('previous').onclick = () => {
         const item = document.querySelector('section div:last-child');
         slider.prepend(item);
-    }
+    };
+
+    document.addEventListener('keyup', (e) => {
+        if (e.key === 'ArrowRight') {
+            const item = document.querySelector('section div:first-child');
+            slider.append(item);
+        }
+
+        if (e.key === 'ArrowLeft') {
+            const item = document.querySelector('section div:last-child');
+            slider.prepend(item);
+        }
+    });
+
+    // automagically
+    setInterval(() => {
+        const item = document.querySelector('section div:first-child');
+        slider.append(item);
+    }, 2000);
 });
