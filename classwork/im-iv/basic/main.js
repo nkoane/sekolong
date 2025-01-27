f3b01da7d69daa31ba0273c01d4c72ab3486998d
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2025-01-27 14:55:41 +0200

    basic javas
    Signed-off-by: lebogang nkoane <1555646+nkoane@users.noreply.github.com>

---
+++ classwork/im-iv/basic/main.js
@@ -0,0 +1,16 @@
+function changeBgColour(colour) {
+  const square = document.querySelector('main div.square');
+  square.style.backgroundColor = colour;
+}
+
+const colourSelector = document.querySelector('main form input[type="color"]#colourSelector');
+
+colourSelector.addEventListener('input', function () {
+  changeBgColour(colourSelector.value);
+});
+
+const toggleModeSelector = document.querySelector('main form input[type="checkbox"]#toggleModeSelector');
+
+toggleModeSelector.addEventListener('change', function () {
+  console.log(toggleModeSelector.checked);
+});
