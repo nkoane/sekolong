64407221f4074143e0242b6a56fc42a61cc2c30c
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2025-02-17 12:02:55 +0200

    im-iv: swapping variables visual implementation
    Signed-off-by: lebogang nkoane <1555646+nkoane@users.noreply.github.com>

---
+++ classwork/im-iv/lebokoso/main.js
@@ -0,0 +1,31 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  /* saving the content of three */
+  const one = document.querySelector('div#one');
+
+  one.addEventListener('dragstart', (event) => {
+    console.log("They see me rollin'");
+  });
+
+  one.addEventListener('dragend', (event) => {
+    console.log("They hatin'");
+  });
+
+  /* read up collision detection; or is it boundry detection */
+
+  /* saving the content of two */
+  const two = document.querySelector('div#two');
+  /* saving the content of three */
+  const three = document.querySelector('div#three');
+  /* saving the content of three */
+  const threeContent = three.innerText;
+
+  setTimeout(() => {
+    return;
+    /* the swap */
+    three.innerText = one.innerText;
+    one.innerText = two.innerText;
+    two.innerText = three.innerText;
+    /* returning the content of three to its original value */
+    three.innerText = threeContent;
+  }, 1000);
+});
