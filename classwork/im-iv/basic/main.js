7d95e38b46b77c957975806d2298b407654cd3ad
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2025-03-31 04:37:09 +0200

    whatever
    Signed-off-by: lebogang nkoane <1555646+nkoane@users.noreply.github.com>

--- classwork/im-iv/basic/main.js
+++ classwork/im-iv/basic/main.js
@@ -1,22 +1,26 @@
 function changeBgColour(colour) {
-  const square = document.querySelector('main div.square');
+  const square = document.querySelector("main div.square");
   square.style.backgroundColor = colour;
 }
 
-const colourSelector = document.querySelector('main form input[type="color"]#colourSelector');
+const colourSelector = document.querySelector(
+  'main form input[type="color"]#colourSelector',
+);
 
-colourSelector.addEventListener('input', function () {
+colourSelector.addEventListener("input", function () {
   changeBgColour(colourSelector.value);
 });
 
-const toggleModeSelector = document.querySelector('main form input[type="checkbox"]#toggleModeSelector');
+const toggleModeSelector = document.querySelector(
+  'main form input[type="checkbox"]#toggleModeSelector',
+);
 
-toggleModeSelector.addEventListener('change', function () {
+toggleModeSelector.addEventListener("change", function () {
   console.log(toggleModeSelector.checked);
 
   if (toggleModeSelector.checked === true) {
-    document.body.classList.add('dark');
+    document.body.classList.add("dark");
   } else {
-    document.body.classList.remove('dark');
+    document.body.classList.remove("dark");
   }
 });
